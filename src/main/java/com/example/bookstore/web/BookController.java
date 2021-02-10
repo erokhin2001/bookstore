391142f537e25f9d9ce96ab818972b33ea83bcf1
tjoma <tjoma@tjomas-MacBook-Pro.local>
2021-02-10 15:06:49 +0200

    addbook.html and editbook.html added, BookController.java has been updated, index.html has been removed

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,10 +1,11 @@
 package com.example.bookstore.web;
 
+import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class BookController {
@@ -12,11 +13,6 @@ public class BookController {
     @Autowired
     private BookRepository bookRepository;
 
-//    @GetMapping("/index")
-//    public String showBooks(Model model) {
-//
-//        return "index";
-//    }
 
     @GetMapping("/booklist")
     public String showBooks(Model model) {
@@ -24,6 +20,27 @@ public class BookController {
         return "booklist";
     }
 
+    @GetMapping("/add")
+    public String addBook(Model model){
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
 
+    @PostMapping("/save")
+    public String saveBook(Book book){
+        bookRepository.save(book);
+        return "redirect:booklist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long bookId) {
+        bookRepository.deleteById(bookId);
+        return "redirect:../booklist";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
+        model.addAttribute("book", bookRepository.findById(bookId));
+        return "editbook";
+    }
 }
-
