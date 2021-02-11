90ce82a68d44424c18f4314ae3b6373daf8c88ca
tjoma <tjoma@tjomas-MacBook-Pro.local>
2021-02-11 15:35:27 +0200

    Category and its repository has been added; removed unused method from BookRepository

--- src/main/java/com/example/bookstore/domain/BookRepository.java
+++ src/main/java/com/example/bookstore/domain/BookRepository.java
@@ -4,8 +4,4 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository extends CrudRepository<Book, Long> {
-
-//    List<Book> findByLastName(String lastName);
-
-}
+public interface BookRepository extends CrudRepository<Book, Long> {}
